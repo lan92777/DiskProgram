a1e71a1b4d24bf96143eea37ac50da700fb033f0
lan92777 <lbq19896823803@163.com>
2024-06-11 14:28:20 +0800

    1.修改文件名称

--- Disk.pro
+++ Disk.pro
@@ -5,7 +5,7 @@
 #-------------------------------------------------
 
 QT       += core gui
-#设置c++编译器版本
+#设置c++编译器版�?
 CONFIG   += c++11
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -16,15 +16,16 @@ TEMPLATE = app
 SOURCES += main.cpp\
         mainwindow.cpp \
     login.cpp \
-    common/logintitle.cpp
+    window_components/1.cpp \
+    selfwindow/logintitle.cpp
 
 HEADERS  += mainwindow.h \
     login.h \
-    common/logintitle.h
+    selfwindow/logintitle.h
 
 FORMS    += mainwindow.ui \
     login.ui \
-    common/logintitle.ui
+    selfwindow/logintitle.ui
 
 RESOURCES += \
     res.qrc
---
+++ selfwindow/logintitle.h
@@ -0,0 +1,64 @@
+#ifndef LOGINTITLE_H
+#define LOGINTITLE_H
+
+#include <QWidget>
+#include <QIcon>
+#include <QToolButton>
+#include <QMouseEvent>
+
+#include<QDebug>
+namespace Ui {
+class logintitle;
+}
+
+class logintitle : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit logintitle(QWidget *parent = 0);
+    ~logintitle();
+
+    /*------------
+    设置图片样式
+    -------------*/
+    //设置图片需要的元素
+    void SetLogOPicture();
+    void settitlename();
+    void SetSetPicture();
+    void SetMinWindowPicture();
+    void SetClsWindowPicture();
+
+    /*------------
+    设置与父窗口的交互
+    -------------*/
+    //设置父窗口指针
+    void setparent(QWidget *parent);
+
+protected:
+
+    //设置鼠标移动窗口事件
+    void mousePressEvent(QMouseEvent *ev);
+    void mouseMoveEvent(QMouseEvent *ev);
+
+
+
+private:
+    //窗口元素是
+    Ui::logintitle *ui;
+    //父窗口元素
+    QWidget *m_parent;
+    //图片元素
+    QPixmap logopicture;
+    QIcon seticon;
+    QIcon closeicon;
+    QIcon minicon;
+    //相对位置坐标点
+    QPoint m_pos;
+    QPoint pos;
+
+signals:
+    void ShowSetWidget();
+    void CloseWindow();
+};
+
+#endif // LOGINTITLE_H
