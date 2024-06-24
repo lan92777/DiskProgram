14149ffab4f1a31e505d409d33f2f30496b767c9
lan92777 <lbq19896823803@163.com>
2024-06-24 20:19:42 +0800

    服务端登陆程序完善

---
+++ disk_server_code/fcgi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+START=1
+STOP=1
+
+
+
--- disk_server_code/src_cgi/login_cgi.c
+++ disk_server_code/src_cgi/login_cgi.c
@@ -1,8 +1,49 @@
-#include <string.h>
+#include"fcgi_config.h"
+#include"fcgi_stdio.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+#define LOGIN_LOG_MODULE "cgi"
+#define LOGIN_LOG_PROC   "login"
+
 
 int main()
 {
-    //堵塞等待用户连接
-    return 0;
+	//堵塞等待用户的连接
+    while(FCGI_Accept()>=0)
+    {
+		//从nginx发送的环境变量以获得数据的长度
+		char *contentLength=getenv("CONTENT_LENGTH");
+  		//设置数据长度定义
+        int len;
+		//token验证的定义
+		char token[128]={0};
+		//告诉nginx处理格式
+		printf("Content-type:text/html\r\n\r\n");
+		//对数据长度进行处理
+		if(contentLength==NULL)
+		{
+			len=0;
+		}
+		else
+		{
+			len=atoi(contentLength);
+		}
+		//对数据长度进行判断
+		if(len<=0)
+		{
+			printf("No data from standard input\n");
 
+			
+		}
+		else
+		{
+			;
+		}
+
+	}
+	return 0;
+	
 }
