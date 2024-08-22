6769853ae168f23811d37d50185d1872e81d42fa
lan92777 <lbq19896823803@163.com>
2024-08-22 18:51:04 +0800

    错误内容修复

--- login.cpp
+++ login.cpp
@@ -104,7 +104,7 @@ login::login(QWidget *parent) :QDialog(parent),ui(new Ui::login)
         //写信息写进配置文件
         com.writeLoginInfo(user,pwd,ui->rember_pwd->isChecked());
         //测试
-        address="192.168.73.128";
+        address="192.168.73.132";
         //address="192.168.73.132";
         port="80";
         //测试
