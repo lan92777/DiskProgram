dfacbfcbdd942d321993f9995a752fab698adcdd
lan92777 <lbq19896823803@163.com>
2024-09-01 20:27:02 +0800

    登陆代码完善

--- disk_server_code/fcgi.sh
+++ disk_server_code/fcgi.sh
@@ -4,4 +4,34 @@ START=1
 STOP=1
 
 
+case $1 in
+	start)
+		START=1
+		STOP=0
+		;;
+	stop)
+		START=0
+		STOP=1
+		;;
+	*)
+		STOP=1
+		START=1
+		;;
+esac
+
+# **********************杀死cgi进程****************
+if [ "$STOP" -eq 1 ];then
+    # 登录
+    kill -9 $(ps aux | grep "./bin_cgi/login" | grep -v grep | awk '{print $2}') > /dev/null 2>&1
+
+	echo "CGI程序已经关闭"
+fi
+
+# **********************启动cgi进程***************
+if [ "$START" -eq 1 ];then
+    # 登录
+    echo -n "登录："
+    spawn-fcgi -a 127.0.0.1 -p 10000 -f ./bin_cgi/login
+	echo  "CGI程序已经启动"
+fi
 
--- disk_server_code/src_cgi/login_cgi.c
+++ disk_server_code/src_cgi/login_cgi.c
@@ -1,49 +1,105 @@
-#include"fcgi_config.h"
-#include"fcgi_stdio.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
+/*
+ *@file login_cgi.c
+ *@brief 登陆后台cgi程序
+ *@author lan
+ *@versio 1.0
+ *@date 2024.9.1
+ */
 
-#define LOGIN_LOG_MODULE "cgi"
-#define LOGIN_LOG_PROC   "login"
+//cgi程序头文件
+#include "fcgi_config.h"
+#include "fcgi_stdio.h"
+//输入输出头文件
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//程序使用配置头文件
+#include "make_log.h"
+#include "util_cgi.h"
+#include <time.h>
+//读取数据库所需要的头文件
+#include "deal_mysql.h"
+#include "redis_op.h"
+//格式化数据所需要的头文件
+#include "cJSON"
+#include "cfg.h"
+//加密所需要的头文件
+#include "des.h"
+#include "base64.h"
+#include "md5.h"
+
+
 
 
 int main()
 {
-	//堵塞等待用户的连接
-    while(FCGI_Accept()>=0)
-    {
-		//从nginx发送的环境变量以获得数据的长度
-		char *contentLength=getenv("CONTENT_LENGTH");
-  		//设置数据长度定义
-        int len;
-		//token验证的定义
+	//堵塞等待用户连接
+	while(FCFI_Accept()>=0)
+	{
+		//接受的含数据长度的字符串数据
+		char *contenLength = getenv("CONTENT_LENGTH");
+		int len;
+		//token变量设置
 		char token[128]={0};
-		//告诉nginx处理格式
+		//告诉浏览器什么类型
 		printf("Content-type:text/html\r\n\r\n");
-		//对数据长度进行处理
-		if(contentLength==NULL)
+		//将字符串数据转变为整数
+		if( contenLength ==NULL )
 		{
-			len=0;
+			Len=0;
 		}
 		else
 		{
 			len=atoi(contentLength);
 		}
-		//对数据长度进行判断
-		if(len<=0)
+		//根据获取的用户信息进行判断
+		if(len <=0)
 		{
-			printf("No data from standard input\n");
-
-			
+			printf("NO data from standard input.<p>\n");
+			//*LOG日志信息	
 		}
 		else
 		{
+			//从web服务器里读取内容
+			char buf[4*1024]={0};
+			int ret=0;
+			ret=fread(buf,1,len,stdin);
+			if(ret==0)
+			{
+				//*LOG日志信息
+				continue;
+			}
+			//*LOG日志信息
+			
+			//获取登陆用户的信息
+			char user[512]={0};
+			char pwd[512]={0};
+			//*将信息从容器里获取
+			//*LOG日志信息
+			//*判断登陆成功与否
+			//登陆成功，生成token
+			if(ret==0)
+			{
 			;
+			}
+			//返回前端登陆信息
+			if(ret == 0)
+			{
+			
+				;
+			}
+			else
+			{
+					;
+			}
+		
 		}
-
-	}
-	return 0;
 	
+	}
+
+
+
+
+
+
 }
